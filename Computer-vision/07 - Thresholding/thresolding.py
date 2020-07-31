06044b09348198ac87aaad6f693191560805db82
Hritik Jaiswal <hritik.jaiswal@somaiya.edu>
2020-07-31 23:48:04 +0530

    CV Updated

--- Computer-vision/07 - Thresholding/thresolding.py
+++ Computer-vision/07 - Thresholding/thresolding.py
@@ -1,8 +1,15 @@
+'''
+Output : Different type of thresolding forms
+'''
+
 import cv2
 import numpy as np
+
 # for image ( we can also do with image )
-##img = cv2.imread('dog.png')
+# img = cv2.imread('dog.png')
+
 cap = cv2.VideoCapture(0)
+
 while True:
     _, frame = cap.read()
     grayscale = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
@@ -12,14 +19,14 @@ while True:
     retval , boo3 = cv2.threshold(grayscale,200,255,cv2.THRESH_TOZERO)
     retval , boo4 = cv2.threshold(grayscale,200,255,cv2.THRESH_TOZERO_INV)
 
-    ##cv2.imshow('frame1',boo1)
-    ##cv2.imshow('frame2',boo2)
-    ##cv2.imshow('frame3',boo3)
-    ##cv2.imshow('frame4',boo4)
-    cv2.imshow('frame',boo)
+    cv2.imshow('boo',boo)
+    cv2.imshow('boo1',boo1)
+    cv2.imshow('boo2',boo2)
+    cv2.imshow('boo3',boo3)
+    cv2.imshow('boo4',boo4)
     cv2.imshow('grayscale',grayscale)
 
-    k = cv2.waitKey(0) & 0xFF
+    k = cv2.waitKey(1) & 0xFF
     if k ==27:
         break
     
