065095c50edce8b4b283373f80fc3e183ab70f01
Hritik Jaiswal <hritik.jaiswal@somaiya.edu>
2020-09-17 13:26:44 +0530

    Added Contributing.md

--- Computer-vision/15 - Sobel and Laplacian Operation/Sobel_Laplacian_Edge_Detechion.py
+++ Computer-vision/15 - Sobel and Laplacian Operation/Sobel_Laplacian_Edge_Detechion.py
@@ -20,11 +20,11 @@ sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)  # y
 
 plt.subplot(2,2,1),plt.imshow(img,cmap = 'gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
-plt.subplot(2,2,2),plt.imshow(laplacian,cmap = 'gray')
-plt.title('Laplacian'), plt.xticks([]), plt.yticks([])
-plt.subplot(2,2,3),plt.imshow(sobelx,cmap = 'gray')
+#plt.subplot(2,2,2),plt.imshow(laplacian,cmap = 'gray')
+#plt.title('Laplacian'), plt.xticks([]), plt.yticks([])
+plt.subplot(2,2,2),plt.imshow(sobelx,cmap = 'gray')
 plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
-plt.subplot(2,2,4),plt.imshow(sobely,cmap = 'gray')
+plt.subplot(2,2,3),plt.imshow(sobely,cmap = 'gray')
 plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
 
 plt.show()
