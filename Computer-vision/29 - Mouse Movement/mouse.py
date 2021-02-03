7a7a8c1ceaaecc37d2f065b3f624cc26b8a4b1e2
Hritik Jaiswal <hritik.jaiswal@somaiya.edu>
2021-02-03 23:46:01 +0530

    Modify mouse

--- Computer-vision/29 - Mouse Movement/mouse.py
+++ Computer-vision/29 - Mouse Movement/mouse.py
@@ -45,44 +45,12 @@ cv2.createTrackbar('width','rad',678,1800,callback)
 while 1:
     _,img=cap.read()
 
-    rad=cv2.getTrackbarPos('rad', 'rad')
-    rad2=cv2.getTrackbarPos('rad2','rad')
-    width=cv2.getTrackbarPos('width', 'rad')
-    cv2.circle(img,(640,640),rad, (0,0,0), width)
-    cv2.ellipse(img,(640,640),(rad,rad2),0,0,360,(0,0,0),width)
-    
     cv2.setMouseCallback('imag',mouse_event)
-    hsv= cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 
+    '''
     edges = cv2.Canny(img,50,150,apertureSize = 3)
     cv2.imshow('edges',edges)
-
-##    minLineLength=50
-##    lines = cv2.HoughLinesP(image=edges,rho=1,theta=np.pi/180, threshold=100,lines=np.array([]), minLineLength=minLineLength,maxLineGap=40)
-##
-##    
-##    a,b,c = lines.shape
-##    for i in range(a):
-##        cv2.line(img, (lines[i][0][0], lines[i][0][1]), (lines[i][0][2], lines[i][0][3]), (0, 0, 255), 3, cv2.LINE_AA)
-##        #cv2.imwrite('houghlines5.jpg',gray)
-    minline=cv2.getTrackbarPos('minline', 'hough')
-    maxgap=cv2.getTrackbarPos('maxgap', 'hough')
-
-    
-    lines = cv2.HoughLines(edges,1, np.pi / 180, 150, None, 0, 0)
-    
-    if lines is not None:
-        for i in range(0, len(lines)):
-            rho = lines[i][0][0]
-            theta = lines[i][0][1]
-            a = math.cos(theta)
-            b = math.sin(theta)
-            x0 = a * rho
-            y0 = b * rho
-            pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
-            pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
-            cv2.line(img, pt1, pt2, (0,0,255), 3, cv2.LINE_AA)
-    
+    '''
     lowh=cv2.getTrackbarPos('lowh', 'trackbar')
     highh=cv2.getTrackbarPos('highh', 'trackbar')
     lows=cv2.getTrackbarPos('lows', 'trackbar')
@@ -93,20 +61,22 @@ while 1:
     lowred=np.array([lowh,lows,lowv])
     highred=np.array([highh,highs,highv])
 
-    mask= cv2.inRange (hsv,lowred,highred)
+    hsv= cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    
+    mask= cv2.inRange(hsv,lowred,highred)
     
     kernel = np.ones((10,10),np.uint8)
     dilute = cv2.dilate(mask, kernel, iterations = 1)
     opening = cv2.morphologyEx(dilute, cv2.MORPH_CLOSE, kernel)
     
-    #cv2.imshow('mask',mask)
-        
+    cv2.imshow('mask',mask)
+    cv2.imshow('opening',opening)    
 
     contours, hierarchy=cv2.findContours(opening,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
     for i in contours:
                     area = cv2.contourArea(i)
-                    if(area>2000):
+                    if(area>1000):
                             x,y,w,h = cv2.boundingRect(i)
 
                             rect = cv2.minAreaRect(i)
@@ -114,7 +84,7 @@ while 1:
                             box = np.int0(box)
                             cv2.drawContours(img,[box],0,(0,0,255),2)
                                                        
-                            #img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+                            #cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
                             cv2.putText(img,"color1 detected",(x,y),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255))
 
 
